d2992131199274e85fdb768c973b46593b7acf01
Ian <ian@right.here>
2018-09-30 09:50:34 +0100

    Make the code more similar by deriving the mushroom position relative to the rabbit position

--- src/main/kotlin/JumpIn.kt
+++ src/main/kotlin/JumpIn.kt
@@ -3,24 +3,24 @@ class JumpIn {
 
     fun solve(vararg board: String): String {
 
-        if ((board[0][2] == 'W' && board[0][1] == 'M') ||
-            (board[4][2] == 'W' && board[4][1] == 'M') ||
-            (board[2][4] == 'W' && board[2][3] == 'M'))
+        if ((board[0][2] == 'W' && board[0][2 - 1] == 'M') ||
+            (board[4][2] == 'W' && board[4][2 - 1] == 'M') ||
+            (board[2][4] == 'W' && board[2][4 - 1] == 'M'))
             return "W(W)"
 
-        if ((board[2][0] == 'W' && board[1][0] == 'M') ||
-            (board[2][4] == 'W' && board[1][4] == 'M') ||
-            (board[4][2] == 'W' && board[3][2] == 'M'))
+        if ((board[2][0] == 'W' && board[2 - 1][0] == 'M') ||
+            (board[2][4] == 'W' && board[2 - 1][4] == 'M') ||
+            (board[4][2] == 'W' && board[4 - 1][2] == 'M'))
             return "W(N)"
 
-        if ((board[0][2] == 'W' && board[0][3] == 'M') ||
-            (board[4][2] == 'W' && board[4][3] == 'M') ||
-            (board[2][0] == 'W' && board[2][1] == 'M'))
+        if ((board[0][2] == 'W' && board[0][2 + 1] == 'M') ||
+            (board[4][2] == 'W' && board[4][2 + 1] == 'M') ||
+            (board[2][0] == 'W' && board[2][0 + 1] == 'M'))
             return "W(E)"
 
-        if ((board[2][0] == 'W' && board[3][0] == 'M') ||
-            (board[2][4] == 'W' && board[3][4] == 'M') ||
-            (board[0][2] == 'W' && board[1][2] == 'M'))
+        if ((board[2][0] == 'W' && board[2 + 1][0] == 'M') ||
+            (board[2][4] == 'W' && board[2 + 1][4] == 'M') ||
+            (board[0][2] == 'W' && board[0 + 1][2] == 'M'))
             return "W(S)"
 
         return ""
