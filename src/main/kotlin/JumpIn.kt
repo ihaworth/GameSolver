60025a7ac35a556bb0c236797afefcd1daf9576c
Ian <ian@right.here>
2018-11-04 13:04:17 +0000

    Extract methods to get the pice N/S/E/W of a given coord.

--- src/main/kotlin/JumpIn.kt
+++ src/main/kotlin/JumpIn.kt
@@ -5,13 +5,18 @@ class JumpIn {
 
         val (rabbitRow, rabbitCol) = locate('W', board)
 
-        if (rabbitCol > 0 && board[rabbitRow    ][rabbitCol - 1] == 'M') return "W" + "(W)"
-        if (rabbitRow > 0 && board[rabbitRow - 1][rabbitCol    ] == 'M') return "W" + "(N)"
-        if (rabbitCol < 4 && board[rabbitRow    ][rabbitCol + 1] == 'M') return "W" + "(E)"
-        if (rabbitRow < 4 && board[rabbitRow + 1][rabbitCol    ] == 'M') return "W" + "(S)"
+        if (rabbitCol > 0 && pieceWest (board, rabbitRow, rabbitCol) == 'M') return "W" + "(W)"
+        if (rabbitRow > 0 && pieceNorth(board, rabbitRow, rabbitCol) == 'M') return "W" + "(N)"
+        if (rabbitCol < 4 && pieceEast (board, rabbitRow, rabbitCol) == 'M') return "W" + "(E)"
+        if (rabbitRow < 4 && pieceSouth(board, rabbitRow, rabbitCol) == 'M') return "W" + "(S)"
 
         return ""
     }
+
+    private fun pieceWest (board: Array<out String>, rabbitRow: Int, rabbitCol: Int) = board[rabbitRow][rabbitCol - 1]
+    private fun pieceNorth(board: Array<out String>, rabbitRow: Int, rabbitCol: Int) = board[rabbitRow - 1][rabbitCol]
+    private fun pieceEast (board: Array<out String>, rabbitRow: Int, rabbitCol: Int) = board[rabbitRow][rabbitCol + 1]
+    private fun pieceSouth(board: Array<out String>, rabbitRow: Int, rabbitCol: Int) = board[rabbitRow + 1][rabbitCol]
 
     private fun locate(piece: Char, board: Array<out String>): Pair<Int, Int> {
         var rabbitRow = -1
