b1f595f186da6f501b1ecdc6c6bfd9b97e0fc52a
Ian <ian@right.here>
2018-11-04 17:10:58 +0000

    Move the range checks to the N/E/S/W methods by embracing null to represent when these places are off the board.

--- src/main/kotlin/JumpIn.kt
+++ src/main/kotlin/JumpIn.kt
@@ -5,15 +5,16 @@ class JumpIn {
 
         val rabbit = locate('W', board)
 
-        if (rabbit.col > 0 && pieceAt(rabbit. west(), board) == 'M') return "W" + "(W)"
-        if (rabbit.row > 0 && pieceAt(rabbit.north(), board) == 'M') return "W" + "(N)"
-        if (rabbit.col < 4 && pieceAt(rabbit. east(), board) == 'M') return "W" + "(E)"
-        if (rabbit.row < 4 && pieceAt(rabbit.south(), board) == 'M') return "W" + "(S)"
+        if (pieceAt(rabbit. west(), board) == 'M') return "W" + "(W)"
+        if (pieceAt(rabbit.north(), board) == 'M') return "W" + "(N)"
+        if (pieceAt(rabbit. east(), board) == 'M') return "W" + "(E)"
+        if (pieceAt(rabbit.south(), board) == 'M') return "W" + "(S)"
 
         return ""
     }
 
-    private fun pieceAt(coord: Coordinate, board: Array<out String>): Char {
+    private fun pieceAt(coord: Coordinate?, board: Array<out String>): Char? {
+        if (coord == null) return null
         return board[coord.row][coord.col]
     }
 
@@ -37,8 +38,8 @@ class JumpIn {
 class Coordinate(val row: Int, val col: Int) {
     constructor(pair: Pair<Int, Int>) : this(pair.first, pair.second)
 
-    fun  west() = Coordinate(row    , col - 1)
-    fun north() = Coordinate(row - 1, col    )
-    fun  east() = Coordinate(row    , col + 1)
-    fun south() = Coordinate(row + 1, col    )
+    fun  west() = if (col == 0) null else Coordinate(row    , col - 1)
+    fun north() = if (row == 0) null else Coordinate(row - 1, col    )
+    fun  east() = if (col == 4) null else Coordinate(row    , col + 1)
+    fun south() = if (row == 4) null else Coordinate(row + 1, col    )
 }
