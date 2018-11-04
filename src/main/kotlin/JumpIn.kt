39b15ccd2346f5c5539e00bb550e096f8fff52fb
Ian <ian@right.here>
2018-11-04 09:25:14 +0000

    Extract variables for the white rabbit location.

--- src/main/kotlin/JumpIn.kt
+++ src/main/kotlin/JumpIn.kt
@@ -3,26 +3,38 @@ class JumpIn {
 
     fun solve(vararg board: String): String {
 
-        if ((board[0][2] == 'W' && board[0][2 - 1] == 'M') ||
-            (board[4][2] == 'W' && board[4][2 - 1] == 'M') ||
-            (board[2][4] == 'W' && board[2][4 - 1] == 'M'))
+        var rabbitRow = -1
+        var rabbitCol = -1
+
+        for (row in 0..4)
+            for (col in 0..4)
+                if (board[row][col] == 'W') {
+                    rabbitRow = row
+                    rabbitCol = col
+                    break
+                }
+
+        if ((board[0][2] == 'W' && board[rabbitRow][rabbitCol - 1] == 'M') ||
+            (board[4][2] == 'W' && board[rabbitRow][rabbitCol - 1] == 'M') ||
+            (board[2][4] == 'W' && board[rabbitRow][rabbitCol - 1] == 'M'))
             return "W(W)"
 
-        if ((board[2][0] == 'W' && board[2 - 1][0] == 'M') ||
-            (board[2][4] == 'W' && board[2 - 1][4] == 'M') ||
-            (board[4][2] == 'W' && board[4 - 1][2] == 'M'))
+        if ((board[2][0] == 'W' && board[rabbitRow - 1][rabbitCol] == 'M') ||
+            (board[2][4] == 'W' && board[rabbitRow - 1][rabbitCol] == 'M') ||
+            (board[4][2] == 'W' && board[rabbitRow - 1][rabbitCol] == 'M'))
             return "W(N)"
 
-        if ((board[0][2] == 'W' && board[0][2 + 1] == 'M') ||
-            (board[4][2] == 'W' && board[4][2 + 1] == 'M') ||
-            (board[2][0] == 'W' && board[2][0 + 1] == 'M'))
+        if ((board[0][2] == 'W' && board[rabbitRow][rabbitCol + 1] == 'M') ||
+            (board[4][2] == 'W' && board[rabbitRow][rabbitCol + 1] == 'M') ||
+            (board[2][0] == 'W' && board[rabbitRow][rabbitCol + 1] == 'M'))
             return "W(E)"
 
-        if ((board[2][0] == 'W' && board[2 + 1][0] == 'M') ||
-            (board[2][4] == 'W' && board[2 + 1][4] == 'M') ||
-            (board[0][2] == 'W' && board[0 + 1][2] == 'M'))
+        if ((board[2][0] == 'W' && board[rabbitRow + 1][rabbitCol] == 'M') ||
+            (board[2][4] == 'W' && board[rabbitRow + 1][rabbitCol] == 'M') ||
+            (board[0][2] == 'W' && board[rabbitRow + 1][rabbitCol] == 'M'))
             return "W(S)"
 
         return ""
     }
+
 }
