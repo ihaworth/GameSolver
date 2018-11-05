315cef1dda9d559b5b461422c744fc5fc625f3ab
Ian <ian@right.here>
2018-11-05 21:35:45 +0000

    Rather large refactoring to use the new Board class more widely and move some behaviour to it.

--- src/main/kotlin/JumpIn.kt
+++ src/main/kotlin/JumpIn.kt
@@ -8,21 +8,19 @@ class JumpIn {
 
     fun solve(vararg board: String): List<String> {
 
-        return listOf(move('W', board) + move('G', board))
+        return listOf(move('W', Board(board)) + move('G', Board(board)))
     }
 
-    private fun move(bunny: Char, board: Array<out String>): String {
-        val rabbit = Board(board).locate(bunny)
+    private fun move(bunny: Char, board: Board): String {
+        val rabbit = board.locate(bunny)
 
-        if (pieceAt(rabbit?.plus(WEST ), board) == 'M') return bunny + "(" + "W" + ")"
-        if (pieceAt(rabbit?.plus(NORTH), board) == 'M') return bunny + "(" + "N" + ")"
-        if (pieceAt(rabbit?.plus(EAST ), board) == 'M') return bunny + "(" + "E" + ")"
-        if (pieceAt(rabbit?.plus(SOUTH), board) == 'M') return bunny + "(" + "S" + ")"
+        if (board.pieceAt(rabbit?.plus(WEST )) == 'M') return bunny + "(" + "W" + ")"
+        if (board.pieceAt(rabbit?.plus(NORTH)) == 'M') return bunny + "(" + "N" + ")"
+        if (board.pieceAt(rabbit?.plus(EAST )) == 'M') return bunny + "(" + "E" + ")"
+        if (board.pieceAt(rabbit?.plus(SOUTH)) == 'M') return bunny + "(" + "S" + ")"
 
         return ""
     }
-
-    private fun pieceAt(coord: Coordinate?, board: Array<out String>) = coord?.validate(Board(board))?.pieceAt(board)
 
 }
 
@@ -38,20 +36,23 @@ class Board(val board: Array<out String>) {
                 }
         return null
     }
+
+    fun pieceAt(coord: Coordinate?): Char? {
+        return validate(coord)?.let { this.board[it.row][it.col] }
+    }
+
+    private fun validate(coord: Coordinate?): Coordinate? {
+        return coord?.let { if (it.row in 0 until height && it.col in 0 until width) it else null }
+    }
+
 }
 
 class Coordinate(val row: Int, val col: Int) {
-    fun validate(board: Board): Coordinate? {
-        if (row in 0 until board.height && col in 0 until board.width)
-            return this
-        return null
-    }
 
     operator fun plus(vector: Vector): Coordinate {
         return Coordinate(row + vector.rowDelta, col + vector.colDelta)
     }
 
-    fun pieceAt(board: Array<out String>) = board[row][col]
 }
 
 class Vector(val rowDelta: Int, val colDelta: Int) {}
